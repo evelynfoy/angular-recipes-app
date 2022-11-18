e9e100cc608eb511f426c5237d5d26f0cdd7c348
Evelyn Foy <evelyn.foy@hotmail.com>
2022-11-18 21:01:39 +0000

    Add loading spinner

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -8,6 +8,7 @@ import { AuthService } from "./auth.service";
 })
 export class AuthComponent {
     isLoginMode = true;
+    isLoading = false;
 
     constructor(private authService: AuthService) {}
 
@@ -22,14 +23,17 @@ export class AuthComponent {
         const email = form.value.email;
         const password = form.value.password;
 
+        this.isLoading = true;
         if (this.isLoginMode) {
             // ...
         } else {
             this.authService.signUp(email, password).subscribe(
                 resData => {
                     console.log(resData);
+                    this.isLoading = false;
                 }, error => {
                     console.log(error);
+                    this.isLoading = false;
                 }
             );
         }
