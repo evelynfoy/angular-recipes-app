d8e7a86c1cdf82697807d6e8501b23235e0c7e39
Evelyn Foy <evelyn.foy@hotmail.com>
2022-11-27 19:04:28 +0000

    Add logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { BehaviorSubject, throwError } from "rxjs";
 import { catchError, tap } from "rxjs/operators";
 import { User } from "./user.model";
@@ -18,7 +19,7 @@ export class AuthService {
 
     user = new BehaviorSubject<User>(null);
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private router: Router) { }
 
     signUp(email: string, password: string) {
         return this.http
@@ -65,6 +66,11 @@ export class AuthService {
                 );
             })
         );
+    }
+
+    logout() {
+        this.user.next(null);
+        this.router.navigate(['/auth']);
     }
 
     private handleAuthentication(
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -33,6 +33,10 @@ export class HeaderComponent implements OnInit , OnDestroy{
         this.dataStorageService.fetchRecipes().subscribe();
     }
 
+    onLogout() {
+        this.authService.logout();
+    }
+
     ngOnDestroy() {
         this.userSub.unsubscribe();
     }
