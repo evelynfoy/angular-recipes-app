10c54961fb1d3d5e395e14cca1e0f512e11bcbe3
Evelyn Foy <evelyn.foy@hotmail.com>
2022-11-27 20:30:13 +0000

    Add auto-login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -68,6 +68,31 @@ export class AuthService {
         );
     }
 
+    autoLogin() {
+        const userData: 
+        {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+            
+        if (!userData) {
+            return;
+        }
+
+        const loadedUser = new User(
+            userData.email, 
+            userData.id, 
+            userData._token, 
+            new Date(userData._tokenExpirationDate)
+        );
+
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+        }
+    }
+
     logout() {
         this.user.next(null);
         this.router.navigate(['/auth']);
@@ -83,6 +108,7 @@ export class AuthService {
         const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
         const user = new User(email, userId, token, expirationDate);
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse) {
