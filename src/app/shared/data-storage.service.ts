9d3c55c62d873eed627de85367e754b9ce8ea474
Evelyn Foy <evelyn.foy@hotmail.com>
2022-11-27 18:56:47 +0000

    Add auth interceptor service

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -27,27 +27,22 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap(user => {
-        return this.http.get<Recipe[]>(
-          'https://ng-complete-guide-b3f60-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
-          {
-            params: new HttpParams().set('auth', user.token)
-          }
-        );
-      }),
-      map(recipes => {
-        return recipes.map(recipe => {
-          return {
-            ...recipe,
-            ingredients: recipe.ingredients ? recipe.ingredients : []
-          };
-        });
-      }),
-      tap(recipes => {
-        this.recipeService.setRecipes(recipes);
-      })
+    return this.http
+    .get<Recipe[]>(
+        'https://ng-complete-guide-b3f60-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+    )
+    .pipe(
+        map(recipes => {
+            return recipes.map(recipe => {
+                return {
+                    ...recipe,
+                    ingredients: recipe.ingredients ? recipe.ingredients : []
+                };
+            });
+        }),
+        tap(recipes => {
+            this.recipeService.setRecipes(recipes);
+        })
     );
   }
 }
